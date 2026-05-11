bba503b7b6bb2dc026231ab02f5b005785ac1c16
purvesh-liv <ppurvesh60@gmail.com>
2026-05-11 01:45:52 -0700

    added clerkprovider middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
@@ -28,9 +35,18 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-mono", jetbrainsMono.variable)}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-mono",
+        jetbrainsMono.variable,
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
     </html>
   );
 }
--- lib/db.ts
+++ lib/db.ts
@@ -11,7 +11,7 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient({
 
 
 
-})
+}) 
 // if its not in production
 if(process.env.NODE_ENV !== "production"){
     globalForPrisma.prisma = prisma
