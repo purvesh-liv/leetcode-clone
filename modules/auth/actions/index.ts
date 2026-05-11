0eb71cb3dd0da617bf17f1bf84cbfa5e3eeee03b
purvesh-liv <ppurvesh60@gmail.com>
2026-05-11 02:30:27 -0700

    added currentuserrole server action

--- modules/auth/actions/index.ts
+++ modules/auth/actions/index.ts
@@ -2,6 +2,8 @@
 
 import { prisma } from "@/lib/db"
 import { currentUser } from "@clerk/nextjs/server"
+import { error } from "console";
+import { useReducer } from "react";
 
 export const onBoardUser = async()=>{
     try {
@@ -37,3 +39,29 @@ export const onBoardUser = async()=>{
         
     }
 }
+
+export const currentUserRole = async () => {
+    try {
+        const user = await currentUser()
+        if(!user){
+            return{
+                success:false,
+                error:"No authenticated user found"
+            }
+        }
+        const {id} = user
+
+        const userRole = await prisma.user.findUnique({
+            where:{
+                clerkId:id
+            },
+            select:{
+                role:true
+            }
+        })
+        return userRole?.role
+    } catch (error) {
+        
+    }
+}
+
